99629ad2e0e70370e5e3caf916ef9d40a2546d72
chandran472 <heisenberg472005@gmail.com>
2025-01-27 19:58:35 +0530

    Time: 0 ms (100%), Space: 11.5 MB (66.92%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int n = nums.size();
+        map<int, int> mp;
+
+        // Count occurrences of each color
+        for (int i = 0; i < n; i++) {
+            mp[nums[i]]++;
+        }
+
+        // Rebuild the array in-place based on the counts in the map
+        int index = 0;
+        for (auto& pair : mp) {
+            int color = pair.first;
+            int count = pair.second;
+            while (count--) {
+                nums[index++] = color;
+            }
+        }
+    }
+};
