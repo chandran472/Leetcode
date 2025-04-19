643dcd8af1ef9ee7969904feb1c02592c8ca355d
chandran472 <heisenberg472005@gmail.com>
2025-04-19 10:00:43 +0530

    Time: 2 ms (42.32%), Space: 11.8 MB (44.43%) - LeetHub

---
+++ 0412-fizz-buzz/0412-fizz-buzz.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<string> fizzBuzz(int n) {
+
+        vector<string>vec;
+        for(int i=1;i<=n;i++)
+        {
+            if(i%3 == 0 && i%5 == 0)
+            {
+                vec.push_back("FizzBuzz");
+            }
+            else if(i%3 == 0) vec.push_back("Fizz");
+            else if(i%5 == 0) vec.push_back("Buzz");
+            else{
+                string ans = to_string(i);
+                vec.push_back(ans);
+            }
+        }
+
+        return vec;
+    }
+
+};
